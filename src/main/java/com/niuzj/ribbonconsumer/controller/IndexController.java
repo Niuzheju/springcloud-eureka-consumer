ee09f356e64d70d21976169565bb16f168331a01
niuzj <2549218666@qq.com>
2019-01-24 17:26:44 +0800

    ok

--- src/main/java/com/niuzj/ribbonconsumer/controller/IndexController.java
+++ src/main/java/com/niuzj/ribbonconsumer/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.niuzj.ribbonconsumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class IndexController {
@@ -12,7 +16,16 @@ public class IndexController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/hello")
-    public String hello(){
-        return restTemplate.getForEntity("http://provider/hello", String.class).getBody();
+    public String hello() {
+        //顺序填充占位符
+//        return restTemplate.getForEntity("http://provider/hello?name={1}", String.class, "nzj").getBody();
+        //使用key-value填充占位符
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "nzj");
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://provider/hello?name={name}", String.class, map);
+        System.out.println("status:" + entity.getStatusCode());
+        System.out.println(entity.getHeaders());
+        return entity.getBody();
+
     }
 }
