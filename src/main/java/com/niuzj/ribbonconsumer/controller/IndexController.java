7845e980ec0c91960f10cf3ee59ff6950610858e
tom <1523892672@qq.com>
2019-01-27 16:26:55 +0800

    ok

--- src/main/java/com/niuzj/ribbonconsumer/controller/IndexController.java
+++ src/main/java/com/niuzj/ribbonconsumer/controller/IndexController.java
@@ -3,12 +3,11 @@ package com.niuzj.ribbonconsumer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +16,12 @@ public class IndexController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    private String url = "http://provider/hello";
+
+    private String url2 = "http://provider/uri";
+
+    private  String result = "success";
 
     /**
      * GET请求
@@ -39,16 +44,34 @@ public class IndexController {
     /**
      * POST请求
      * 把参数放到请求体里
+     * 请求体可以传入数据或者HttpEntity,数据可以封装在HttpEntity中
+     * 服务端接受也必须使用@RequestBody注解,用实体类或者Map接收
      */
     @RequestMapping("/hello2")
-    public String hello2(){
-        Map<String, String> map = new HashMap<>();
+    public String hello2() {
+        Map<String, Object> map = new HashMap<>();
         map.put("name", "nzj");
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setDate(System.currentTimeMillis());
-        HttpEntity<Map<String, String>> httpEntity = new HttpEntity<>(map, httpHeaders);
+        httpHeaders.set("airen", "zyq");
+        HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(map, httpHeaders);
+//
+//        ResponseEntity<String> entity = restTemplate.postForEntity("http://provider/hello", httpEntity, String.class);
+//        return entity.getBody();
 
-        ResponseEntity<String> entity = restTemplate.postForEntity("http://provider/hello", httpEntity, String.class);
-        return entity.getBody();
+//        return restTemplate.postForObject(url, httpEntity, String.class);
+
+        //从header中提取Location参数构造URI
+        URI uri = restTemplate.postForLocation(url2, httpEntity);
+        System.out.println(uri);
+        return "success";
+    }
+
+    /**
+     * PUT请求
+     */
+    @RequestMapping("/put")
+    public String put(){
+        restTemplate.put("http://provider/user/{1}", String.class, "2");
+        return result;
     }
 }
